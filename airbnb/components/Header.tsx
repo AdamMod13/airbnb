11ddf7655ca43c10e30ba6ef6f71a18a1c31bbfc
Modlich Adam <adam.modlich13@gmail.com>
2023-02-09 18:01:17 +0100

    AirBnb-8: Added functionality to aplication header

--- airbnb/components/Header.tsx
+++ airbnb/components/Header.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import Image from 'next/image';
-import { Bars3Icon, GlobeAltIcon, MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import { Bars3Icon, GlobeAltIcon, MagnifyingGlassIcon, UserCircleIcon, UsersIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { DateRangePicker } from 'react-date-range';
 
 export default function Header () {
+	const [searchInput, setSearchInput] = useState('');
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date())
+	const [numberOfGuests, setNumberOfGuests] = useState(1);
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: 'selection'
+	}
+
+	const handleSelect = (ranges: any): void => {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	}
+
+	const resetInput = (): void => {
+		setSearchInput('');
+	}
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
 			{/* Left */}
@@ -18,7 +42,13 @@ export default function Header () {
 
 			{/* Middle - Search*/}
 			<div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-				<input type="text" placeholder='start you search...' className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'/>
+				<input 
+					type="text" 
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
+					placeholder='start you search...' 
+					className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'	
+				/>
 				<MagnifyingGlassIcon className='hidden md:inline-flex h-7 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
 			</div>
 
@@ -31,6 +61,44 @@ export default function Header () {
 					<UserCircleIcon className='h-6'/>
 				</div>
 			</div>
+			
+			{searchInput && (
+				<div className='flex flex-col col-span-3 mx-auto'>
+					<DateRangePicker 
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#FD5B61"]}
+						onChange={handleSelect}
+					/>
+					<div className='flex items-center border-b mt-4'>
+						<h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+
+						<UsersIcon className='h-5'/>
+						<input 
+							type="number" 
+							value={numberOfGuests}
+							min={1}
+							onChange={(e) => setNumberOfGuests(parseInt(e.target.value))}
+							className='w-12 pl-2 text-lg outline-none text-red-400'	
+						/>
+					</div>
+					<div className='flex mt-3'>
+						<button 
+							className='flex-grow text-gray-500'
+							onClick={resetInput}
+						>
+							Cancel
+						</button>
+						<button 
+							className='flex-grow text-red-400'
+							// onClick={closeDate}
+						>
+							Search
+						</button>
+					</div>
+				</div>
+			)}
+			
     </header>
   );
 }
