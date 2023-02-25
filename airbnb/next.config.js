7d8eba4545384e5f67d66ab60b1cb010fb875279
Modlich Adam <adam.modlich13@gmail.com>
2023-02-25 18:00:13 +0100

    AirBnb-14: Packages

--- airbnb/next.config.js
+++ airbnb/next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['links.papareact.com'],
+  },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoiZXJja3VzIiwiYSI6ImNsZTM0OTd4ZTA1N24zb2x4Ym5penp6Y3oifQ.7IGSUl4RGRTVtVbiflmiBw'
   }
 }
